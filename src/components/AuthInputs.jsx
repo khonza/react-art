2296af62546b0135f205b3fbf2cb3841c1899099
Kholekile Somagaca <kholekile.s@gmail.com>
2025-02-14 08:05:02 +0200

    🐛fix(auth-inputs): prefix prop name

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -15,17 +15,17 @@ const ControlLabel = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color:${ (props) => props.invalid ? '#f87171' : '#6b7280'};
+  color:${ (props) => props.$invalid ? '#f87171' : '#6b7280'};
 `;
 
 const ControlInput = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${ (props) => props.invalid ? '#fed2d2' : '#d1d5db'} ; 
-  color: ${ (props) => props.invalid ? '#ef4444' : '#374151'};
+  background-color: ${ (props) => props.$invalid ? '#fed2d2' : '#d1d5db'} ; 
+  color: ${ (props) => props.$invalid ? '#ef4444' : '#374151'};
   border: 1px solid transparent;
-  border-color: ${ (props) => props.invalid ? '#f73f3f' : 'transparent'};
+  border-color: ${ (props) => props.$invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
@@ -54,14 +54,14 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <ControlLabel invalid={emailNotValid}>Email</ControlLabel>
-          <ControlInput type="email" invalid={emailNotValid}
+          <ControlLabel $invalid={emailNotValid}>Email</ControlLabel>
+          <ControlInput type="email" $invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <ControlLabel invalid={passwordNotValid}>Password</ControlLabel>
-          <ControlInput type="password" invalid={passwordNotValid}
+          <ControlLabel $invalid={passwordNotValid}>Password</ControlLabel>
+          <ControlInput type="password" $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
